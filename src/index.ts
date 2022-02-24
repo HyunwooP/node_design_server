74c22a8337d607add4b19a539401b4918719953c
harry <awakelife93@gmail.com>
2022-02-24 22:06:13 +0900

    로깅 삭제

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,14 @@ import App from "./App";
 
 (async () => {
   try {
-    /**
-     * Create Route Item
-     */
     await App.onCreateRoute();
 
-    /**
-     * Create HTTP Server
-     */
     await App.onCreateServer();
 
-    /**
-     * Connect DB
-     */
     await App.onConnectDB();
 
-    /**
-     * Connect TypeOrm Mysql Repositorys
-     */
     await App.onConnectRepository();
 
-    /**
-     * Create Mysql Sample Data
-     */
     await App.onCreateTestSample();
   } catch (error: unknown) {
     console.log(error);
