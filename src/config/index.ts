418c403c6fd56dda5d2463832937aaeeffcb3102
hw.park <hw.park@fastfive.co.kr>
2022-03-05 16:06:15 +0900

    node env 추가

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
  * 외부에 노출되는 데이터들이기 때문에 주의해서 사용해야 한다.
  */
 export default {
+  // "development" | "production" | "localhost"
   NODE_ENV: process.env.NODE_ENV ?? "development",
   port: process.env.port ?? 3002,
   mongoPort: process.env.mongoPort ?? 27017,
--- src/lib/database/config/index.ts
+++ src/lib/database/config/index.ts
@@ -2,21 +2,34 @@ import env from "@/config";
 import { MongoConnectionOptions } from "typeorm/driver/mongodb/MongoConnectionOptions";
 
 export type TypeOrmConfigType = {
-  [index: string]: MongoConnectionOptions;
+  [key: string]: MongoConnectionOptions;
 };
 
 /**
  * Typeorm Mongo config
  */
 export const mongoConfig: TypeOrmConfigType = {
-  development: {
-    name: "development",
+  localhost: {
+    name: "localhost",
     type: "mongodb",
     host: "127.0.0.1",
     port: 27017,
     username: "root",
     password: env.mongoPassword,
     database: "localDB",
+    synchronize: true,
+    logging: false,
+    dropSchema: true,
+    entities: ["src/models/**/*.ts"],
+  },
+  development: {
+    name: "development",
+    type: "mongodb",
+    host: "",
+    port: undefined,
+    username: "",
+    password: "",
+    database: "",
     synchronize: true,
     logging: false,
     dropSchema: true,
