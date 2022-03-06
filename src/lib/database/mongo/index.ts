59b11aa48497973c5e834ebbeb7bcc503d44a33f
hw.park <hw.park@fastfive.co.kr>
2022-03-06 12:27:33 +0900

    try/catch 제거

--- src/lib/database/mongo/index.ts
+++ src/lib/database/mongo/index.ts
@@ -4,14 +4,10 @@ import { createConnection } from "typeorm";
 import { mongoConfig } from "../config";
 
 export const connectMongo = async (): Promise<void> => {
-  try {
-    await createConnection({
-      ...mongoConfig[env.NODE_ENV],
-      useUnifiedTopology: true,
-    });
-  } catch (error: unknown) {
-    console.log(`connectMongo Connect Failed!! ${error}`);
-  }
+  await createConnection({
+    ...mongoConfig[env.NODE_ENV],
+    useUnifiedTopology: true,
+  });
 };
 
 export const connectRepository = (): void => {
