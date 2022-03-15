c663994fec7d7dec04d12fc5349cec6e84028961
harry <awakelife93@gmail.com>
2022-03-15 20:40:06 +0900

    app 객체 내에서 시스템 제어 하게끔...

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,6 @@
 import { Application } from "express";
+import * as _ from "lodash";
+import config from "./config";
 import {
   AppRepository,
   connectMongo,
@@ -42,28 +44,52 @@ class App {
     await AppRepository.generateTestData();
   }
 
-  public onCreateDevelopmentApp = async (): Promise<void> => {
+  public onCreateLocalHostApp = async (): Promise<void> => {
     this.onCreateRoute();
-
     this.onCreateServer();
-
     await this.onConnectDB();
-
     await this.onConnectRepository();
-
     await this.onCreateTestSample();
   };
 
+  // * localhost환경과 달라야 할 경우 확장
+  private onCreateDevelopmentApp = this.onCreateLocalHostApp;
+
   public onCreateProductionApp = async (): Promise<void> => {
     this.onInitializeSentry();
-
     this.onCreateRoute();
-
     this.onCreateServer();
-
     await this.onConnectDB();
-
     await this.onConnectRepository();
+  };
+
+  private getApplication = (): Function => {
+    const applications: {
+      [environment: string]: Function;
+    } = {
+      production: this.onCreateProductionApp,
+      development: this.onCreateDevelopmentApp,
+      localhost: this.onCreateLocalHostApp,
+    };
+
+    const application = applications[config.NODE_ENV];
+
+    if (_.isFunction(application)) {
+      console.log(`NODE_ENV =======> ${config.NODE_ENV} Start`);
+      return application;
+    } else {
+      console.log(`NODE_ENV is Undefined!!! So, LocalHost Mode Start`);
+      return this.onCreateLocalHostApp;
+    }
+  };
+
+  public startApplication = async (): Promise<void> => {
+    try {
+      const application = this.getApplication();
+      await application();
+    } catch (error: unknown) {
+      console.log(error);
+    }
   };
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 import "module-alias/register";
 import App from "./App";
-import config from "./config";
 
-(async () => {
-  try {
-    console.log(`NODE_ENV =======> ${config.NODE_ENV} Start`);
-
-    if (config.NODE_ENV === "production") {
-      await App.onCreateProductionApp();
-    } else {
-      await App.onCreateDevelopmentApp();
-    }
-  } catch (error: unknown) {
-    console.log(error);
-  }
-})();
+App.startApplication();
