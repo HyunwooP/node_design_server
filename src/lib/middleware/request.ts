820b8fd73e322ae81d7a9f22122777e2608205f2
harry <awakelife93@gmail.com>
2022-04-02 20:43:40 +0900

    request params도 item에 집어넣는다.

--- src/lib/middleware/request.ts
+++ src/lib/middleware/request.ts
@@ -10,7 +10,7 @@ const createItem = (request: IRequest): void => {
   switch (request.method) {
     case "GET":
     case "DELETE":
-      const query: UnknownObject = { ...request.query };
+      const query: UnknownObject = { ...request.query, ...request.params };
 
       // Mysql과 다르게 Mongo는 take, skip을 number타입으로 무조건 제공해야함.
       if (!_.isEmpty(query.take)) query.take = Number(query.take);
@@ -22,7 +22,7 @@ const createItem = (request: IRequest): void => {
     case "POST":
     case "PUT":
     case "PATCH":
-      const body: UnknownObject = { ...request.body };
+      const body: UnknownObject = { ...request.body, ...request.params };
       request.item = body;
       break;
   }
