ef1d8380fde867caa0e2aab1e58e34c5df7d2215
hw.park <hw.park@fastfive.co.kr>
2022-04-30 20:52:06 +0900

    type 추가로 인한 쓸모없는 주석 제거

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,6 @@ import "dotenv/config";
  * 외부에 노출되는 데이터들이기 때문에 주의해서 사용해야 한다.
  */
 const config: ConfigType = {
-  // "development" | "production" | "localhost"
   NODE_ENV: (process.env.NODE_ENV as NodeEnvType) ?? "localhost",
   sentryDSN: process.env.sentryDSN ?? "",
   port: process.env.port ?? 3005,
