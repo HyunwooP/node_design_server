f119b4ff255a2f5b3adbea479953b7156be3b992
harry <awakelife93@gmail.com>
2022-05-18 14:14:09 +0900

    config mongo  확장

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,19 @@
 import { NodeEnvType } from "@/lib/type";
 import "dotenv/config";
+import { MongoConnectionOptions } from "typeorm/driver/mongodb/MongoConnectionOptions";
+
+type MongoCustomConfigType = {
+  [env in NodeEnvType]: Pick<
+    MongoConnectionOptions,
+    "port" | "host" | "username" | "database" | "password"
+  >;
+};
 
 export type ConfigType = {
   NODE_ENV: NodeEnvType;
   sentryDSN: string;
   port: string | number;
-  mongoUserName: string;
-  mongoPort: string | number;
-  mongoHost: string;
-  mongoPassword: string;
-  mongoDataBase: string;
+  mongo: MongoCustomConfigType;
   origin: string;
   timezone: string;
 };
@@ -21,11 +25,35 @@ const config: ConfigType = {
   NODE_ENV: (process.env.NODE_ENV as NodeEnvType) ?? "localhost",
   sentryDSN: process.env.sentryDSN ?? "",
   port: process.env.port ?? 3005,
-  mongoUserName: process.env.mongoUserName ?? "root",
-  mongoPort: process.env.mongoPort ?? 27017,
-  mongoHost: process.env.mongoHost ?? "127.0.0.1",
-  mongoPassword: process.env.mongoPassword ?? "",
-  mongoDataBase: process.env.mongoDataBase ?? "localDB",
+  mongo: {
+    localhost: {
+      port: process.env.mongoLostHostPort
+        ? Number(process.env.mongoLostHostPort)
+        : 27017,
+      host: process.env.mongoLocalHost ?? "127.0.0.1",
+      username: process.env.mongoLocalHostUserName ?? "root",
+      password: process.env.mongoLocalHostPassword ?? "",
+      database: process.env.mongoLocalHostDataBase ?? "localDB",
+    },
+    development: {
+      port: process.env.mongoDevelopmentPort
+        ? Number(process.env.mongoDevelopmentPort)
+        : 27017,
+      host: process.env.mongoDevelopmentHost ?? "",
+      username: process.env.mongoDevelopmentUserName ?? "",
+      password: process.env.mongoDevelopmentPassword ?? "",
+      database: process.env.mongoDevelopmentDataBase ?? "",
+    },
+    production: {
+      port: process.env.mongoProductionPort
+        ? Number(process.env.mongoProductionPort)
+        : 27017,
+      host: process.env.mongoProductionHost ?? "",
+      username: process.env.mongoProductionUserName ?? "",
+      password: process.env.mongoProductionPassword ?? "",
+      database: process.env.mongoProductionDataBase ?? "",
+    },
+  },
   origin:
     process.env.gatewayDomain && process.env.gatewayPort
       ? `http://${process.env.gatewayDomain}:${process.env.gatewayPort}`
