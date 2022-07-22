803e13f8451d62bb37db2072c0bbb7f6230a88bd
hw.park <hw.park@fastfive.co.kr>
2022-07-22 14:48:19 +0900

    response status 추가

--- src/lib/route/index.ts
+++ src/lib/route/index.ts
@@ -1,7 +1,7 @@
 import { getErrorItem } from "@/utils";
 import { Application } from "express";
 import _ from "lodash";
-import { initializeRouteLevelMiddleWare, IRequest, IResponse } from "..";
+import { CommonStatusCode, initializeRouteLevelMiddleWare, IRequest, IResponse } from "..";
 import RouteItems, { RouteItemType } from "./items";
 
 export default (app: Application): void => {
@@ -13,12 +13,12 @@ export default (app: Application): void => {
         try {
           const result = await item.next(request, response);
           console.log(`SUCCESS_${_.toUpper(item.method)}_${item.path}`);
+          response.status(result.status ?? CommonStatusCode.OK);
           response.send({
             item: result,
           });
         } catch (error: unknown) {
           const _error = getErrorItem(error);
-
           console.log(`ERROR_${_.toUpper(item.method)}_${item.path}`);
           console.log(_error);
           response.status(_error.status);
